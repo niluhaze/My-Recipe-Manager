8efbc59f64b38dc402eafd6e869933289a41de00
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 14:00:15 +0200

    added edit page

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
+//dev requirements
+require("dotenv").config()
+
+//specify requirements
 const express = require("express")
-const app = express();
+const app = express()
+
+const mongoose = require("mongoose")
+
+app.use(express.json()) //let server accept json
+
+//connect to mongoDB database on localhost
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}) 
+const db = mongoose.connection
+db.on("error",(error) => console.error(error)) //log errors in console
+db.once("open", () => console.log("Established connection to MongoDB")) //log when connection to database is established
 
 app.set("view engine", "ejs") //set to use ejs as the view engine
 
+//redirect "/" to "/my-recipes"
 app.get("/", (req, res) => {
     res.redirect("/my-recipes")
 })
 
+//create and apply routings for the different pages
+const editRouter = require("./routes/edit")
 const myRecipesRouter = require("./routes/my-recipes")
 const recipeRouter = require("./routes/recipe")
 
+app.use("/edit", editRouter)
 app.use("/my-recipes", myRecipesRouter)
 app.use("/recipe", recipeRouter)
 
-app.listen(3000)
+app.listen(3000, () => console.log("Server Started"))
