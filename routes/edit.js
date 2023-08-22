04463f13d5756af56aec190f7392808f2471615c
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 14:46:14 +0200

    added seperate empty url ending to edit router

--- routes/edit.js
+++ routes/edit.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+router.get("/", (req, res) => {
+    res.render("edit")
+})
+
 router.get("/:url_name", (req, res) => {
     res.render("edit")
 })
