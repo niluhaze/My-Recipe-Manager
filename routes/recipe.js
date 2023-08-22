16836b40796a91ec447b70871e68e9bbb14688d7
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 16:01:59 +0200

    for testing: return url_name

--- routes/recipe.js
+++ routes/recipe.js
@@ -3,12 +3,13 @@ const router = express.Router()
 const Recipe = require("../models/recipe")
 
 //redirect unspecified recipe url to /myrecipes
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
     res.redirect("/my-recipes")
 })
 
-router.get("/:url_name", async (req, res) => {
-    res.render("recipe")
+router.get("/:url_name", (req, res) => {
+    //for testing: return url_name
+    res.send(req.params.url_name)
 })
 
 module.exports = router
