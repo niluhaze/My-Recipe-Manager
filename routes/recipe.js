9b3d5a4a8bb3f7743a2c10d72f45fba9adb0e584
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 14:44:59 +0200

    added recipe model requirement

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
+const Recipe = require("../models/recipe")
 
-router.get("/:url_name", (req, res) => {
+router.get("/:url_name", async (req, res) => {
     res.render("recipe")
 })
 
