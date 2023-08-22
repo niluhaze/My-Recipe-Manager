34e6841f30fc4288d6d1aa84578d8a8a64b5e190
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 15:44:51 +0200

    added redirect to my-recipes if recipe unspecified

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,6 +2,11 @@ const express = require("express")
 const router = express.Router()
 const Recipe = require("../models/recipe")
 
+//redirect unspecified recipe url to /myrecipes
+router.get("/", async (req, res) => {
+    res.redirect("/my-recipes")
+})
+
 router.get("/:url_name", async (req, res) => {
     res.render("recipe")
 })
