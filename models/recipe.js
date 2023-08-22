7707bd9dc1b20dcb3fc0759fb110c0ab4918f164
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 13:57:21 +0200

    added mongoose schema

---
+++ models/recipe.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+const recipeSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    cookTimeTotal:{
+        type: Number,
+        required: true
+    },
+    cookTimeActive:{
+        type: number,
+        required: true
+    },
+    dateAdded:{
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    tags:{
+        type: Array,
+        required: true,
+        default: []
+    },
+    ingredients:{
+        type: Array,
+        required: true,
+    },
+    instructions:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("Recipe", recipeSchema)
