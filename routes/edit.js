3139d22949ee2f1314d2d07408f9ca513a1f544c
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 16:46:41 +0200

    added post method to edit router

--- routes/edit.js
+++ routes/edit.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const Recipe = require("../models/recipe")
 
 router.get("/", (req, res) => {
     res.render("edit")
@@ -8,5 +9,30 @@ router.get("/", (req, res) => {
 router.get("/:url_name", (req, res) => {
     res.render("edit")
 })
+
+router.post("/", async (req, res) => {
+    const recipe = new Recipe({
+        url_name: req.body.url_name,
+        name: req.body.name,
+        cookTimeTotal: req.body.cookTimeTotal,
+        cookTimeActive: req.body.cookTimeActive,
+        dateAdded: req.body.dateAdded,
+        tags: req.body.tags,
+        ingredients: req.body.ingredients,
+        instructions: req.body.instructions
+    })
+    try {
+        const newRecipe = await recipe.save()
+        res.status(201).json(newRecipe)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+/* TBD 
+function nameToUrlName(name){
+
+    return name
+} */
 
 module.exports = router
