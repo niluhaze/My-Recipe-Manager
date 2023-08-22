37cf4b6235cfb0a63003ee6a9bd355d971c71aa3
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-22 16:09:47 +0200

    my-recipes now transfers all data from db

--- routes/my-recipes.js
+++ routes/my-recipes.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const router = express.Router()
+const Recipe = require("../models/recipe")
 
-router.get("/", (req, res) => {
-    res.render("my-recipes")
+router.get("/", async (req, res) => {
+    //try to get all recipes
+    try {
+        const recipes = await Recipe.find()
+        res.json(recipes)
+    } catch (error) {
+        //send error code 500 in case of server-side error
+        res.status(500).json({message: error.message})
+    }
 })
 
 module.exports = router
