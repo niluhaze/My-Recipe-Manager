10cbe998fc2f4a5ba4510382e7ae7ffb3c81c3ec
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-23 16:29:58 +0200

    Use script from database.js

--- routes/my-recipes.js
+++ routes/my-recipes.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const Recipe = require("../models/recipe")
+const Scripts = require ("../scripts/database")
 
 router.get("/", async (req, res) => {
     //try to get all recipes
     try {
         const recipes = await Recipe.find()
-        res.json(recipes)
+        res.json(Scripts.getRecipeListData(req.query))
     } catch (error) {
         //send error code 500 in case of server-side error
         res.status(500).json({message: error.message})
