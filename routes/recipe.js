fc2d5416427c7bb0ec5466873e9c492ad4b998eb
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-25 15:04:34 +0200

    now use middleware to retrieve data

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const router = express.Router()
 const Recipe = require("../models/recipe")
+const getRecipe = require("../scripts/getRecipe")
 
-//redirect unspecified recipe url to /myrecipes
+//redirect unspecified recipe url to /my-recipes
 router.get("/", (req, res) => {
     res.redirect("/my-recipes")
 })
 
-router.get("/:urlName", (req, res) => {
-    //for testing: return urlName
-    res.send(req.params.urlName)
+router.get("/:urlName", getRecipe, async (req, res) => {
+    //get data via middleware function and send
+    res.send(res.recipe)
 })
 
 module.exports = router
