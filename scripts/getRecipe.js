6a8d18b72afe4f0b90660d16bcc3f88954d47b68
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-25 13:45:11 +0200

    fixed bug

--- scripts/getRecipe.js
+++ scripts/getRecipe.js
@@ -3,13 +3,13 @@ A general middleware function which gets a recipe based on its urlName.
 Used to avoid duplicate code for simple requests.
 */
 
-const recipe = require("../models/recipe")
 const Recipe = require("../models/recipe")
 
 async function getRecipe (req, res, next) {
+    let recipe
     try {
         //retrieve data of single recipe by urlName
-        let recipe = await Recipe.find({urlName: req.params.urlName}).limit(1)
+        recipe = await Recipe.find({"urlName": req.params.urlName}).limit(1)
         //respond with 404 error if no recipe by that name found
         if (recipe == null){
             return res.status(404).json({message: `No recipe found with urlName "${req.params.urlName}".`})
