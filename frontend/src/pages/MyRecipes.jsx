7647a30aefc29ecb1d47cd8ef8bd88ad82820448
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-08-31 10:49:57 +0200

    Preparing for query form

--- frontend/src/pages/MyRecipes.jsx
+++ frontend/src/pages/MyRecipes.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
+
 export function MyRecipes(){
   return (
     <>
       <h1>MyRecipes</h1>
+      <form className="query-form">
+        
+      </form>
     </>
   )
 }
