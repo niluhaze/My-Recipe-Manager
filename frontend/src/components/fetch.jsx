c8550f4db351b82c8269bfad045e9db2bc4e6ae5
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-01 16:14:44 +0200

    created general get request method

---
+++ frontend/src/components/fetch.jsx
@@ -0,0 +1,20 @@
+import { useQuery, useMutation } from "@tanstack/react-query"
+import axios from "axios"
+
+HOST_PATH = "http://localhost:3000"
+
+//makes an http GET request to the backend with the given queryKey and to the given urlPath
+export function doGetQuery(qKey, urlPath = "/") {
+
+  const query = useQuery({
+    queryKey: [qKey],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        HOST_PATH + urlPath)
+      return data
+    }
+  })
+
+  return query
+
+}
