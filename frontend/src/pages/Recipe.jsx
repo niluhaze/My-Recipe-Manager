f3180a94d1638cf89ff7e4df34e1afc106505a29
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-04 09:46:21 +0200

    Added new doGetQuery function to recipe page

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -1,3 +1,19 @@
+import { useParams } from "react-router-dom"
+import { doGetQuery } from "../components/query"
+
 export function Recipe(){
-  return <h1>Recipe</h1>
+
+  const { urlName } = useParams()
+
+  const recipeQuery = doGetQuery("recipe", ("/recipe/" + urlName))
+
+  if (recipeQuery.isLoading) return <h1>Loading...</h1>
+  if (recipeQuery.isError) return <pre>{JSON.stringify(recipeQuery.error)}</pre>
+
+  return (
+    <>
+      <h1>Recipe</h1>
+      <h2>{JSON.stringify(recipeQuery.data)}</h2>
+    </>
+  )
 }
