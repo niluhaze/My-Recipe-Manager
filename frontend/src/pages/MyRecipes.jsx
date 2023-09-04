35873c36ffb29af84fa42ba8ff6de03552c225a5
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-04 09:48:58 +0200

    added temporary useParams test

--- frontend/src/pages/MyRecipes.jsx
+++ frontend/src/pages/MyRecipes.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react"
+import { useParams } from "react-router-dom"
 import { useQuery, useMutation } from "@tanstack/react-query"
+import { doGetQuery } from "../components/query"
 import axios from "axios"
 
+
 export function MyRecipes(){
+
+  const test = useParams()
+  console.log(test)
 
   const recipesQuery =  useQuery({
     queryKey: ["my-recipes"],
@@ -19,6 +25,7 @@ export function MyRecipes(){
   return (
     <>
       <h1>MyRecipes</h1>
+      <h2>{}</h2>
 
       <form className="query-form">
 
