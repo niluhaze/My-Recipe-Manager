3dc104e541e1e13f9035b54d17130e2e89a2ab2f
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-06 09:42:26 +0200

    added redirects

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,9 @@
+import { Navigate } from "react-router-dom"
+
 export function Home(){
   return (
     <>
-      <h1>Hello World!</h1>
-      <h1>You shouldn't be able to see this (in the future).</h1>
+      <Navigate to="my-recipes"/>
     </>
   )
 }
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 export function NotFound(){
-  return <h1>404 Page not found.</h1>
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/my-recipes")
+    }, 2000)
+  }, [])
+
+  return (
+    <>
+      <h1 className="text-3xl p-3">404 - Page not found.</h1>
+    </>
+  )
 }
