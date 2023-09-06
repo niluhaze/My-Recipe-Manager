4f3fa02519dee38f69b1fef72c387444866dad9a
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-06 16:19:00 +0200

    added responsiveness to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,31 +1,41 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
- 
+
 const Navbar = () => {
+
+  //toggle visibility of the navbar links when clicking on the hamburger button
+  useEffect(() => {
+    const navbarToggleButton = document.getElementById("navbar-toggle")
+    const navbarLinks = document.getElementById("navbar-links")
+
+    navbarToggleButton.addEventListener("click", () => 
+    navbarLinks.classList.toggle("hidden"))
+  })
+
   return (
-    <nav className="h-20 text-center whitespace-nowrap overflow-hidden bg-primary text-background">
+    <nav className="min-h-[80px] flex flex-col md:flex-row md:text-center justify-between bg-primary text-background">
       {/* Logo */}
-      <div className="float-left h-full aspect-square p-3 -ml-1">
+      <div className="h-20 aspect-square p-3 -ml-1 md:absolute">
         <Link to="/">
-          <svg className="h-full w-full fill-background" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 50 50">
+          <svg className="h-full fill-background" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
             <path d="M12 39h32V2H12C8.691 2 6 4.691 6 8v34.417C6 45.496 8.691 48 12 48h32v-2H12c-2.168 0-4-1.641-4-3.583C8 40.501 9.757 39 12 39zm24.709-7.294a.997.997 0 0 1-1.414.002l-6.523-6.494-1.76 1.76-1.846-1.879 3.153-3.153 8.387 8.349c.391.39.393 1.023.003 1.415zM16.286 10.007l7.733 7.781-3.044 3.044L16.23 16a4.199 4.199 0 0 1 .056-5.993zm-1.957 20.286 13.024-13.024c-.034-.085-.083-.163-.107-.252-.399-1.509-.322-3.426 1.045-4.777 2.031-2.094 5.497-2.989 6.998-1.505 1.501 1.571.596 4.909-1.435 6.916-1.444 1.428-3.298 1.545-4.8 1.16-.104-.027-.196-.081-.294-.122L14.743 31.707c-.195.195.549.293.293.293s-.512-.098-.707-.293a.999.999 0 0 1 0-1.414z"/>
           </svg>
         </Link>
       </div>
-      {/* Tabs */}
-      <div className="h-full inline-block">
-        <div className="h-full text-2xl flex">
-          <Link className="hover:bg-primary-highlight p-3 flex flex-col justify-center text-center" to="/my-recipes">RECIPES</Link>
-          <Link className="hover:bg-primary-highlight p-3 flex flex-col justify-center text-center" to="/saved">SAVED</Link>
-          <Link className="hover:bg-primary-highlight p-3 flex flex-col justify-center text-center" to="/edit">ADD</Link>
+      {/* Links */}
+      <div id="navbar-links" className="hidden md:inline-block bg-primary-light md:bg-transparent md:absolute md:top-0 md:right-1/2 md:translate-x-1/2">
+        <div className="text-2xl flex flex-col md:flex-row">
+          <Link className="md:hover:bg-primary-light p-2 md:py-6" to="/my-recipes">RECIPES</Link>
+          <Link className="md:hover:bg-primary-light p-2 md:py-6" to="/saved">SAVED</Link>
+          <Link className="md:hover:bg-primary-light p-2 md:py-6" to="/edit">ADD</Link>
         </div>
       </div>
       {/* Hamburger menu button */}
-      <div className="float-right h-full aspect-square p-3 -mr-1 hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <path className="h-full w-full fill-background" d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z"/>
+      <button id="navbar-toggle" className="h-20 absolute top-0 right-0 md:static md:hidden aspect-square p-4 -mr-1">
+        <svg className="h-full fill-background" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z"/>
         </svg>
-      </div>
+      </button>
       
     </nav>
   )
