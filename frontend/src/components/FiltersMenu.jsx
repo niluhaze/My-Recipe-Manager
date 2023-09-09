6328ccac3473b98a20be620d9695b40d1ef1a87a
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-09 08:20:52 +0200

    added dynamic filters menu

--- frontend/src/components/FiltersMenu.jsx
+++ frontend/src/components/FiltersMenu.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
 
-const FiltersMenu = () => {
+const FiltersMenu = ({ toggle, setToggle }) => {
   return (
-    <div className="">FiltersMenu</div>
+    <>
+      {/* Cancel button */}
+      <div className="md:hidden flex justify-end mt-2 mx-2">
+        <button onClick={e => setToggle( !toggle )} className="h-10 aspect-square fill-black" >
+          {/* X icon */}
+          <svg className="h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3l105.4 105.3c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256l105.3-105.4z"/>
+          </svg>
+        </button>
+      </div>
+      <form className=" min-w-[200px] flex flex-col p-2 gap-2">
+        <button className="p-1 rounded font-semibold text-lg text-components bg-primary hover:bg-primary-light active:bg-primary-light-light">
+          Apply
+        </button>
+        <div>
+          <div className="font-semibold text-lg">Sort by</div>
+          <select>
+            <option value="-dateAdded">Newest</option>
+            <option value="dateAdded">Oldest</option>
+            <option value="cookTimeActive">Fastest (active)</option>
+            <option value="-cookTimeActive">Longest (active)</option>
+            <option value="cookTimeTotal">Fastest (total)</option>
+            <option value="-cookTimeTotal">Longest (total)</option>
+          </select>
+        </div>
+        <div className="flex flex-col">
+          <div className="font-semibold text-lg">Type</div>
+          <label>
+            <input type="checkbox" name="tags" id="check-appetizer" /> Appetizer
+          </label>
+          <label>
+            <input type="checkbox" name="tags" id="check-main-course" /> Main course
+          </label>
+          <label>
+            <input type="checkbox" name="tags" id="check-side-dish" /> Side dish
+          </label>
+          <label>
+            <input type="checkbox" name="tags" id="check-dessert" /> Dessert
+          </label>
+          <label>
+            <input type="checkbox" name="tags" id="check-baking" /> Baking
+          </label>
+          <div className="font-semibold text-lg">Tags</div>
+          <label>
+            <input type="checkbox" name="tags" id="check-vegan" /> Vegan
+          </label>
+          <label>
+            <input type="checkbox" name="tags" id="check-asian" /> Asian
+          </label>
+          <label>
+            <input type="checkbox" name="tags" id="check-oriental" /> Oriental
+          </label>
+        </div>
+
+      </form>
+    </>
   )
 }
 
--- frontend/src/pages/MyRecipes.jsx
+++ frontend/src/pages/MyRecipes.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useQuery, useMutation } from "@tanstack/react-query"
 import { doGetQuery } from "../scripts/query"
@@ -8,29 +8,56 @@ import FiltersMenu from "/src/components/FiltersMenu"
 
 export function MyRecipes(){
 
-  //needed to rerender page when user types in search bar
-  const [searchString, setSearchString] = useState('')
-
   // const test = useParams()
   // console.log(test)
 
   const recipesQuery = doGetQuery("my-recipes", "/my-recipes?sortBy=cookTimeTotal&sortDir=-1")
 
-  if(recipesQuery.isLoading) return <h1>Loading...</h1>
-  if(recipesQuery.isError) return <pre>{JSON.stringify(recipesQuery.error)}</pre>
+  /* Variables and functions for the page's dynamic elements */
+
+  //needed to rerender page when user types in search bar
+  const [searchString, setSearchString] = useState('')
+
+  //toggle visibility of the filter menu (for small screens)
+  const [ isFirst, setIsFirst ] = useState( true ) //for making sure the toggle doesn't trigger on mount
+  const [ toggle, setToggle ] = useState( false ) //toggle whether the filter menu is shown or hidden
+
+  //run this whenever toggle changes
+  useEffect(() => {
+    //make sure the code after if block does't get run on mount
+    if( isFirst ) {
+      setIsFirst( false )
+      return
+    }
+    document.getElementById("filters-menu").classList.toggle("hidden")
+    document.getElementById("filters-blur").classList.toggle("hidden")
+  }, [ toggle ] )
+
+
+
+  /* Page outputs */
+
+  //output loading and error messages when fetching recipe data
+  if( recipesQuery.isLoading ) return <h1>Loading recipes...</h1>
+  if( recipesQuery.isError ) return <pre>{JSON.stringify(recipesQuery.error)}</pre>
 
   return (
     <>
       <div className="max-w-full flex justify-center">
-        <div className="z-20 hidden md:block absolute md:static h-auto md:max-w-xs bg-components md:drop-shadow">
-          <FiltersMenu/>
+        {/* Filters menu */}
+        <div id="filters-menu" className="z-20 hidden md:block absolute left-0 top-0 bottom-0 md:static h-auto md:max-w-xs bg-components drop-shadow">
+          <FiltersMenu toggle={toggle} setToggle={setToggle}/>
         </div>
+        {/* When filters menu is active:
+            Add a blur behind menu to obscure the recipes behind it
+            and to prevent accidental redirects when misclicking a recipe while setting filters */}
+        <div id="filters-blur" className="z-10 hidden md:hidden backdrop-blur-sm absolute top-0 bottom-0 left-0 right-0"></div>
         <div className="max-w-[1400px]">
           {/* Wrapper for filter menu button and search bar */}
           <div className="flex justify-center p-5 gap-3">
             {/* Filter menu toggle button (for small view) */}
             <div className="md:hidden h-10 aspect-square bg-components rounded-full drop-shadow hover:drop-shadow-lg">
-              <button className="h-full w-full p-2">
+              <button onClick={e => setToggle( !toggle )} className="h-full w-full p-2" >
                 {/* Sliders icon */}
                 <svg className="fill-neutral-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                   <path d="M0 416c0 17.7 14.3 32 32 32h54.7c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H233.3c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48H32c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1-64 0zm192-160a32 32 0 1 1 64 0 32 32 0 1 1-64 0zm32-80c-32.8 0-61 19.7-73.3 48H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h246.7c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48H480c17.7 0 32-14.3 32-32s-14.3-32-32-32h-54.7c-12.3-28.3-40.5-48-73.3-48zm-160-48a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48H32C14.3 64 0 78.3 0 96s14.3 32 32 32h86.7c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H265.3z"/>
@@ -42,7 +69,7 @@ export function MyRecipes(){
               <input type="search" placeholder="Search recipes..." className="outline-none bg-transparent"
                 value={searchString} onChange={e => setSearchString(e.target.value)}
               />
-              <button >x</button>
+              {/* <button >x</button> */}
               <button className="h-6 aspect-square mx-1">
                 {/* Magnifying glass icon */}
                 <svg className="fill-neutral-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
