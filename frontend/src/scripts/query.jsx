57ff62270d38045e83e2bfe38606ff26f0a2a93f
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-12 14:48:38 +0200

    use useMutation for post request

--- frontend/src/scripts/query.jsx
+++ frontend/src/scripts/query.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const HOST_PATH = "http://localhost:3000";
 
-//makes an http GET request to the backend with the given queryKey and to the given urlPath
+// makes an http GET request to the backend with the given queryKey and to the given urlPath
 export function doGetQuery(qKey, urlPath) {
   const query = useQuery({
     queryKey: [qKey],
@@ -12,19 +12,15 @@ export function doGetQuery(qKey, urlPath) {
       return data;
     },
   });
-
   return query;
 }
 
-//makes an http POST request to the backend with the given queryKey, to the given urlPath and with the given data
-export function doPostQuery(qKey, urlPath, data) {
-  const query = useQuery({
-    queryKey: [qKey],
-    queryFn: async () => {
-      const response = await axios.post(HOST_PATH + urlPath, data);
-      return response;
+// makes an http POST request to the backend with the given queryKey, to the given urlPath and with the given data
+export function doPostQuery(urlPath) {
+  const mutation = useMutation({
+    mutationFn: (data) => {
+      return axios.post(HOST_PATH + urlPath, data);
     },
   });
-
-  return query;
+  return mutation;
 }
