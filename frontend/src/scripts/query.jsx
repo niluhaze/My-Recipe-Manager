0cfbb51780282956860b06f14f87bfecfb4143fe
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-12 10:56:55 +0200

    formatted and added post request function

--- frontend/src/scripts/query.jsx
+++ frontend/src/scripts/query.jsx
@@ -1,20 +1,30 @@
-import { useQuery, useMutation } from "@tanstack/react-query"
-import axios from "axios"
+import { useQuery, useMutation } from "@tanstack/react-query";
+import axios from "axios";
 
-const HOST_PATH = "http://localhost:3000"
+const HOST_PATH = "http://localhost:3000";
 
 //makes an http GET request to the backend with the given queryKey and to the given urlPath
 export function doGetQuery(qKey, urlPath) {
-
   const query = useQuery({
     queryKey: [qKey],
     queryFn: async () => {
-      const { data } = await axios.get(
-        HOST_PATH + urlPath)
-      return data
-    }
-  })
+      const { data } = await axios.get(HOST_PATH + urlPath);
+      return data;
+    },
+  });
 
-  return query
+  return query;
+}
 
+//makes an http POST request to the backend with the given queryKey, to the given urlPath and with the given data
+export function doPostQuery(qKey, urlPath, data) {
+  const query = useQuery({
+    queryKey: [qKey],
+    queryFn: async () => {
+      const promise = await axios.post(HOST_PATH + urlPath, data);
+      return promise;
+    },
+  });
+
+  return query;
 }
