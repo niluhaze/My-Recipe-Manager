705d4200f2a6c6cfc30bb437ad083f0610ff9f9c
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-13 15:55:08 +0200

    edit: exported minutesToTime

--- frontend/src/components/RecipeTile.jsx
+++ frontend/src/components/RecipeTile.jsx
@@ -1,18 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-
-// time is stored in minutes, this converts it to hours and minutes
-const minutesToTime = (minutes) => {
-  //add leading zeroes to a number to make it 2 digits or more
-  const addLeadingZeroes = (number) => {
-    number = String(number)
-    //if number's length is less than 2 add leading zeros to make it 2 digits, else leave as is
-    return (number.length < 2) ? ('00'+number).slice(-2) : number
-  }
-  const newMinutes = minutes % 60;
-  const newHours = minutes - newMinutes;
-  return `${addLeadingZeroes(newHours)}:${addLeadingZeroes(newMinutes)}`;
-}
+import { minutesToTime } from "../scripts/minutesToTime"
 
 const RecipeTile = (query) => {
 
