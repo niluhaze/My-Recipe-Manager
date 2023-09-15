8354f838fa0caff850e59a9bc6c52ef7b55b7c83
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-15 15:56:33 +0200

    fix: error 404 trigger

--- backend/scripts/getRecipe.js
+++ backend/scripts/getRecipe.js
@@ -15,14 +15,14 @@ async function getRecipe (req, res, next) {
         //retrieve data of single recipe by urlName
         recipe = await Recipe.find({"urlName": req.params.urlName}, VARS_FOR_RECIPE_PAGE).limit(1)
         //respond with 404 error if no recipe by that name found
-        if (recipe == null){
+        if (recipe.length == 0){
             return res.status(404).json({message: `No recipe found with urlName "${req.params.urlName}".`})
         }
     } catch (error) { 
         //catch and respond to server side error
         return res.status(500).json({message: error.message})
     }
-    res.recipe = recipe //add subscriber to response object
+    res.recipe = recipe //add recipe to response object
     next() //move on to next piece of middleware or response method
 }
 
