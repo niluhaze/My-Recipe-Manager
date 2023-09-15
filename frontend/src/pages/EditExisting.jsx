f8742c81dee55e3d1588879a05daecd1c016f784
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-15 16:02:48 +0200

    add: edit existing path

---
+++ frontend/src/pages/EditExisting.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { doGetQuery } from "../scripts/query";
+import { Edit } from "../components/Edit";
+
+export const EditExisting = () => {
+  const { urlName } = useParams(); // get accessed urlName from /edit/urlName
+  const recipeQuery = doGetQuery("edit-recipe", "/recipe/" + urlName);
+
+  // show if loading data
+  if (recipeQuery.isLoading) return <h1>Loading...</h1>;
+  // show if error occured
+  if (recipeQuery.isError) {
+    try {
+      console.log(recipeQuery.error.response.status);
+      if (recipeQuery.error.response.status == 404)
+        return <Navigate to="/not-found" />;
+    } catch (error) {
+      console.log(error);
+    }
+    return <pre>{JSON.stringify(recipeQuery.error)}</pre>;
+  }
+
+  // after recipe data has been queried successfully:
+  const existingData = recipeQuery.data[0];
+
+  return (
+    /* 
+      A unique key tells React to rerender Edit,
+      otherwise it might show old data when editing different recipes after each other.
+    */
+    <Edit key={existingData.urlName + Date.now()} existingData={existingData}></Edit>
+  );
+};
