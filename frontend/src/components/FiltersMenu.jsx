50ebfc98154173f76f12d8514726fd150e3b9133
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-18 15:45:02 +0200

    edit: import tags from json file

--- frontend/src/components/FiltersMenu.jsx
+++ frontend/src/components/FiltersMenu.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react"
+import allTags from "../assets/tags.json"
 
 const FiltersMenu = ({ toggle, setToggle }) => {
   return (
@@ -16,46 +17,57 @@ const FiltersMenu = ({ toggle, setToggle }) => {
         <button className="p-1 rounded font-semibold text-lg text-components bg-primary hover:bg-primary-light active:bg-primary-light-light">
           Apply
         </button>
+        {/* Sort By dropdown */}
         <div>
           <div className="font-semibold text-lg">Sort by</div>
           <select>
             <option value="-dateAdded">Newest</option>
             <option value="dateAdded">Oldest</option>
-            <option value="cookTimeActive">Fastest (active)</option>
-            <option value="-cookTimeActive">Longest (active)</option>
             <option value="cookTimeTotal">Fastest (total)</option>
             <option value="-cookTimeTotal">Longest (total)</option>
+            <option value="cookTimeActive">Fastest (active)</option>
+            <option value="-cookTimeActive">Longest (active)</option>
           </select>
         </div>
-        <div className="flex flex-col">
-          <div className="font-semibold text-lg">Type</div>
-          <label>
-            <input type="checkbox" name="tags" id="check-appetizer" /> Appetizer
-          </label>
-          <label>
-            <input type="checkbox" name="tags" id="check-main-course" /> Main course
-          </label>
-          <label>
-            <input type="checkbox" name="tags" id="check-side-dish" /> Side dish
-          </label>
-          <label>
-            <input type="checkbox" name="tags" id="check-dessert" /> Dessert
-          </label>
-          <label>
-            <input type="checkbox" name="tags" id="check-baking" /> Baking
-          </label>
-          <div className="font-semibold text-lg">Tags</div>
-          <label>
-            <input type="checkbox" name="tags" id="check-vegan" /> Vegan
-          </label>
-          <label>
-            <input type="checkbox" name="tags" id="check-asian" /> Asian
-          </label>
-          <label>
-            <input type="checkbox" name="tags" id="check-oriental" /> Oriental
-          </label>
+        {/* Tags */}
+        <div>
+          <div className="text-lg font-semibold">Tags</div>
+          {/* 
+          allTags stores tags like this:
+          {
+            category1: [tag01, tag02, ...]
+            category2: [tag11, tag12, ...]
+            ...
+          }
+          create sections of tags with categoryN as headline...
+          */}
+          {Object.keys(allTags).map((category) => (
+            <div key={"key-category-" + category}>
+              <div className="m-1 text-neutral-800">{category}</div>
+              <div className="flex flex-col gap-2">
+                {/* ...and then create the corresponding tag checkmarks below: */}
+                {allTags[category].map((tag) => (
+                  <div key={"key-tag-" + tag}>
+                    <input
+                      className="peer/check hidden"
+                      type="checkbox"
+                      name="tags"
+                      id={"id-" + tag}
+                      value={tag}
+                      // defaultChecked={false}
+                    />
+                    <label
+                      className="px-2 py-[2px] rounded-full bg-neutral-400 text-components peer-checked/check:bg-primary"
+                      htmlFor={"id-" + tag}
+                    >
+                      {tag}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
-
       </form>
     </>
   )
