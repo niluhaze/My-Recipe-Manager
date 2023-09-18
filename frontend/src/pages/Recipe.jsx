c06a4488f5364d63dfb59ee4d5fc1cc969cef229
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-18 14:27:34 +0200

    add: ajustable ingredient amounts

--- frontend/src/pages/Recipe.jsx
+++ frontend/src/pages/Recipe.jsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { minutesToTime } from "../scripts/time";
+import { multiplyNumbersInStringBy } from "../scripts/multiplyNumbersInStringBy";
 
-export function Recipe({data}) {
+export function Recipe({ data }) {
+  const [ingredients, setIngredients] = useState(data.ingredients);
+
+  const handleQuantityChange = (event) => {
+    event.preventDefault(); // prevent page reload on submit
+
+    const newQuantity = event.target.form[0].value; // get new quantity from form
+
+    // multiply all numbers in instructions to match new quantity and apply new ingredient list to page
+    const newIngredients = multiplyNumbersInStringBy(data.ingredients, (newQuantity / data.quantity));
+    setIngredients(newIngredients)
+  };
 
   let date = data.dateAdded; // Date stored in format yyyy-mm-ddThh:mm:ss.msZ
   date = date.slice(0, date.lastIndexOf("T")); // remove everything after "T"
@@ -12,23 +25,24 @@ export function Recipe({data}) {
       <div className="w-full max-w-3xl md:mx-auto md:m-4 md:rounded-2xl shadow bg-components">
         {/* Image */}
         <div className="w-full aspect-[4/3] flex flex-col justify-center items-center border-b">
-          {(data.image != undefined && data.image != "") ?
+          {data.image != undefined && data.image != "" ? (
             <img
               className="object-cover w-full h-full md:rounded-t-2xl"
               src={data.image}
               alt="No image found"
             />
-            :
+          ) : (
             <p className="text-neutral-500">No image found</p>
-          }
-          
+          )}
         </div>
         {/* Content wrapper */}
         <div className="break-words flex flex-col gap-2 m-2">
           {/* Title and Edit button wrapper */}
           <div className="flex justify-between">
             {/* Title */}
-            <p className="break-words font-semibold text-4xl">{data.recipeName}</p>
+            <p className="break-words font-semibold text-4xl">
+              {data.recipeName}
+            </p>
             {/* Edit button */}
             <Link to={"/edit/" + data.urlName}>
               {/* Edit icon */}
@@ -60,16 +74,21 @@ export function Recipe({data}) {
             <div>{date}</div>
           </div>
           {/* Tags */}
-          <div className="flex flex-wrap gap-1">
-            {data.tags.map((tagName) => (
-              <p
-                className="px-2 rounded-full bg-primary text-components"
-                key={tagName}
-              >
-                {tagName}
-              </p>
-            ))}
-          </div>
+          {(data.tags.length > 0) ?
+            <div className="flex flex-wrap gap-1 border-b border-neutral-300 pb-2">
+              {data.tags.map((tagName) => (
+                <p
+                  className="px-2 rounded-full bg-primary text-components"
+                  key={tagName}
+                >
+                  {tagName}
+                </p>
+              ))}
+            </div>
+          :
+            null
+          }
+          
           {/* Quantity */}
           <form className="flex gap-2">
             <input
@@ -82,10 +101,17 @@ export function Recipe({data}) {
               className="w-12"
             />
             <label htmlFor="quantity">{data.quantityUnit}</label>
+            <button
+              type="submit"
+              className="px-2 rounded text-components bg-primary hover:bg-primary-light active:bg-primary-light-light"
+              onClick={handleQuantityChange}
+            >
+              Apply
+            </button>
           </form>
           {/* Ingredients */}
           <p className="text-lg font-semibold">Ingredients</p>
-          <p className="whitespace-pre-line">{data.ingredients}</p>
+          <p className="whitespace-pre-line">{ingredients}</p>
           {/* Instructions / Body */}
           <p className="text-lg font-semibold">Instructions</p>
           <p className="whitespace-pre-line">{data.body}</p>
---
+++ frontend/src/scripts/multiplyNumbersInStringBy.jsx
@@ -0,0 +1,73 @@
+// import { toDecimal } from "vulgar-fractions";
+
+export const multiplyNumbersInStringBy = (string, multiplyBy) => {
+  //boolean storing whether start / end of a number has been found
+  let hasFoundNumerStart = false;
+  let hasFoundNumberEnd = false;
+  // integer storing start and end points of found number
+  let numberStartIndex;
+  let numberEndIndex;
+  // string with value "float" / "fraction" / "unclear"
+  let foundNumberType;
+
+  const replaceWithAtPostition = (replaceWith, start, end) => {
+    return (
+      string.substring(0, start) +
+      replaceWith +
+      string.substring(end, string.length)
+    );
+  };
+
+  const whenDigitFound = () => {
+    // extract the number ending at the found index
+    // match digits as well as the chars . , / in the same line
+    let number = String(
+      /[\d\.,\/]+$/s.exec(string.substring(0, numberEndIndex))
+    );
+    // save start index of number for reference later
+    numberStartIndex = numberEndIndex - number.length;
+    //remove any leading slashes, as there should be a number before a slash in a fraction
+    number = number.replace(/^[\/]+/, "");
+    // get number of chars in number which are not digits
+    const nonDigitCharsAmount = (number.match(/\.,\//g) || []).length;
+    //if there is more than one non digit, number is neither recognized as a floating point number nor fraction
+    if (nonDigitCharsAmount > 1) return;
+
+    // convert the stringified number to a float number
+    const slashesAmount = (number.match(/\//g) || []).length;
+    if (slashesAmount === 1) {
+      // if number is a fraction, convert to float
+      const fractionParts = number.split("/");
+      number = Number(fractionParts[0]) / Number(fractionParts[1]);
+    } else {
+      // if number is float, replace comma with period and convert to number
+      number = Number(number.replace(",", "."));
+    }
+
+    // multiply number
+    number = number * multiplyBy;
+
+    //replace number in text
+    string = replaceWithAtPostition(number, numberStartIndex, numberEndIndex);
+  };
+
+  // let charToDecimal;
+  let charAtIndex;
+  for (let index = string.length - 1; index >= 0; index--) {
+    // go through string from end to start until a digit has been found
+    charAtIndex = string.charAt(index);
+    // TODO: handle unicode fractions
+    /* charToDecimal = toDecimal(charAtIndex); // convert any unicode fraction chars with their decimal representations, e.g.: ⅓ => 0.3333333333333333
+    if (charAtIndex != charToDecimal) {
+
+    } */
+    if (/[0-9]/.test(charAtIndex)) {
+      // if char at index is a digit
+      numberEndIndex = index + 1; // set end of found number to index
+      whenDigitFound();
+      index = numberStartIndex; //skip chars where there can't be a (new) number
+    }
+  }
+
+  return string;
+};
