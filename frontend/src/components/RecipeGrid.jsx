897e762e50316352e438902ee9e53f8b05d01de8
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-19 18:00:05 +0200

    add: no recipes found condition

--- frontend/src/components/RecipeGrid.jsx
+++ frontend/src/components/RecipeGrid.jsx
@@ -12,7 +12,10 @@ export const RecipeGrid = ({ queryString }) => {
   if (recipesQuery.isLoading) return <h1>Loading recipes...</h1>;
   if (recipesQuery.isError)
     return <pre>{JSON.stringify(recipesQuery.error)}</pre>;
-
+  if (recipesQuery.data.length === 0) 
+    return (
+      <h1 className="w-auto">Could not find recipes matching the applied filters</h1>
+    );
   return (
     // map data to recipe tiles and display as grid
     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 m-3">
