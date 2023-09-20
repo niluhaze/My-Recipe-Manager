6a2ca13bcd4236a2ea526836d72e654093409233
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-20 10:41:02 +0200

    edit: clean-up

--- backend/routes/edit.js
+++ backend/routes/edit.js
@@ -1,41 +1,15 @@
+/* 
+  Routes for editing existing and creating new recipes.
+  "/edit" 
+*/
+
 const express = require("express");
 const router = express.Router();
 const Recipe = require("../models/recipe");
 const getRecipe = require("../scripts/getRecipe");
 const database = require("../scripts/database");
 
-//functions for creating new recipe under "/":
-//open edit page to create new recipe
-router.get("/", (req, res) => {
-  res.json({ message: "edit page" });
-});
-
-//post a new recipe
-router.post("/", async (req, res) => {
-  const newUrlName = await database.getNewUrlName(req.body.recipeName);
-  const newDate = Date.now();
-
-  const recipe = new Recipe({
-    urlName: newUrlName,
-    recipeName: req.body.recipeName,
-    cookTimeTotal: req.body.cookTimeTotal,
-    cookTimeActive: req.body.cookTimeActive,
-    dateAdded: newDate,
-    tags: req.body.tags,
-    quantity: req.body.quantity,
-    quantityUnit: req.body.quantityUnit,
-    body: req.body.body,
-    image: req.body.image,
-  });
-  try {
-    const newRecipe = await recipe.save();
-    res.status(201).json({ urlName: newRecipe.urlName });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-//post a new recipe
+// route for posting a new recipe under "/edit":
 router.post("/", async (req, res) => {
   const newUrlName = await database.getNewUrlName(req.body.recipeName);
   const newDate = Date.now();
@@ -62,27 +36,13 @@ router.post("/", async (req, res) => {
   }
 });
 
-//functions for editing existing recipe under "/urlName":
-//open edit page of existing recipe
-router.get("/:urlName", getRecipe, async (req, res) => {
-  //get data via middleware function and send
-  res.send(res.recipe);
-});
-
-//edit an existing recipe
+// route for editing an existing recipe under "/edit/urlName":
 router.post("/:urlName", async (req, res) => {
   const urlName = req.params.urlName;
-  console.log("urlName:", urlName);
-  /* 
-    Check if the name was changed to require a new urlName.
-    If the urlName from the post request url doesn't match the urlName from the request body,
-    the title was edited and a new urlName needs to be generated, else it stays the same.
-  */
-  const urlNameFromReq = database.convertToUrlName(req.body.recipeName);
-  let newUrlName = urlName;
-  if (!urlName.includes(urlNameFromReq)) {
-    newUrlName = await database.getNewUrlName(urlNameFromReq);
-  }
+  const newUrlName = await database.getNewUrlNameAfterEdit( // urlName can change when title changes
+    req.body.recipeName, // new recipe name
+    urlName // old url name
+  );
   console.log("edit-existing:", urlName, "to", newUrlName);
   try {
     // find the recipe with the matching (potentially old) urlName and update it with recipe
@@ -110,6 +70,7 @@ router.post("/:urlName", async (req, res) => {
   }
 });
 
+// route for deleting a specific recipe under "/edit/urlName":
 router.delete("/:urlName", async (req, res) => {
   try {
     if (await database.checkIfUrlNameExists(req.params.urlName)) {
