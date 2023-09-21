92c44e6976642576e11fc01867f8c8705f2b01ce
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-21 15:51:38 +0200

    edit: make get req with search params from url

--- frontend/src/components/RecipeGrid.jsx
+++ frontend/src/components/RecipeGrid.jsx
@@ -3,27 +3,35 @@
   Used on "/my-recipes" page.
 */
 
+import { useSearchParams } from "react-router-dom";
 import { doGetQuery } from "../scripts/query";
 import RecipeTile from "/src/components/RecipeTile";
 
-export const RecipeGrid = ({ queryString }) => {
+export const RecipeGrid = () => {
+  // get the query string from the url
+  const [searchParams] = useSearchParams();
+  const queryString = searchParams.toString();
+
+  // build queryUrl and use it to make GET request to backend
   let queryUrl = "/my-recipes";
   queryUrl += queryString.length > 0 ? "?" + queryString : "";
   const recipesQuery = doGetQuery("my-recipes", queryUrl);
 
-  // output loading and error messages when fetching recipe data
+  // loading message
   if (recipesQuery.isLoading) return <h1>Loading recipes...</h1>;
+  // error message
   if (recipesQuery.isError)
     return <pre>{JSON.stringify(recipesQuery.error)}</pre>;
+  // no data found message
   if (recipesQuery.data.length === 0)
     return (
-      <div className=" flex justify-items-center">
-        <p>Could not find any recipes matching the applied filters.</p>
-      </div>
+      <p className="">
+        Could not find any recipes matching the applied filters.
+      </p>
     );
   return (
     // map data to recipe tiles and display as grid
-    <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 p-3">
       {recipesQuery.data.map((tileData) => (
         <RecipeTile key={tileData.urlName} data={tileData} className="w-full" />
       ))}
