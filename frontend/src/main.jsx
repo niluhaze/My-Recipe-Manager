df77ccf4604cc7c3c2498c2e7d2ebbf307236648
niluhaze <140706048+niluhaze@users.noreply.github.com>
2023-09-29 12:43:56 +0200

    edit: disabled react query devtools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
-        <ReactQueryDevtools />
+        {/* <ReactQueryDevtools /> */}
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
